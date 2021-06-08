b38ecc177039198765314bc135083ad2f1f4fc1a
alonbenjo <83594519+alonbenjo@users.noreply.github.com>
2021-06-09 02:17:39 +0300

    Add files via upload

--- ExamDetails.h
+++ ExamDetails.h
@@ -4,54 +4,60 @@
 
 #define MONTH_LENGTH 30
 
-#include <xstring>
+#include <iostream>
 
-using std::string ; // Should we use the using in the h file?
-static const
+namespace mtm {
 
-class ExamDetails {
-    int course;
-    int exam_month;///Enum?
-    int exam_day;
-    double exam_hour; ///Should it be double? something else?
-    int exam_length;
-    string exam_link;
-
-public:
-    ExamDetails(const int course,const int examMonth,const int examDay,const double examHour,const int examLength, const string &examLink);
-    ExamDetails(const ExamDetails &other_exam_details);
-    ExamDetails& operator=(const ExamDetails& exam_details);
-
-    bool operator<(const ExamDetails &other) const;
-    int operator-(const ExamDetails &other) const;
+    using std::string; // Should we use the using in the h file?
 
 
-private:
-    ~ExamDetails()=default;
+    class ExamDetails {
+        int course;
+        int exam_month;///Enum?
+        int exam_day;
+        double exam_hour; ///Should it be double? something else?
+        int exam_length;
+        string exam_link;
+        static double fraction(double num); //??? private
 
-};
 
-bool ExamDetails::operator<(const ExamDetails &other) const {
-    if (exam_month < other.exam_month)
-        return true;
-    if (other.exam_month < exam_month)
-        return false;
-    if (exam_day < other.exam_day)
-        return true;
-    if (other.exam_day < exam_day)
-        return false;
-    return exam_hour<other.exam_hour;
+    public:
+        ExamDetails(int course, int exam_month, int exam_day, double exam_hour,
+                    int exam_length, const string &exam_link = "");
+
+        ExamDetails(const ExamDetails &other_exam_details);
+
+        ExamDetails &operator=(const ExamDetails &exam_details);
+
+        friend bool operator<(const ExamDetails &exam1, const ExamDetails &exam2);
+
+        int operator-(const ExamDetails &other) const;
+
+        static ExamDetails makeMatamExam();
+
+        const string &getLink() const;
+
+        void setLink(const string &examLink);
+
+        friend std::ostream &operator<<(std::ostream &os, const ExamDetails &details);
+
+        class InvalidDateException {
+        };
+
+        class InvalidTimeException {
+        };
+
+        class InvalidArgsException {
+        };
+
+        //TODO check for static consts
+        //static const double  EXAM_HOUR = 13.5;
+        ~ExamDetails() = default;
+
+    //private:
+
+
+    };
+
 }
-int ExamDetails::operator-(const ExamDetails &other) const{
-    return (MONTH_LENGTH*exam_month +exam_day) - (MONTH_LENGTH*other.exam_month +other.exam_day);
-}
-
-
-ExamDetails& ExamDetails::operator=(const ExamDetails& exam_details){
-    if(this == &exam_details){
-        return *this;
-    }
-
-}
-
 #endif //UNTITLED2_EXAMDETAILS_H
